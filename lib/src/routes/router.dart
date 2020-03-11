7390b283a1e69a41a7a7fe7f6efdc6ac3e0a4c4d
Paulo Saldanha da Silva <paulosaldanhadasilva@gmail.com>
2020-03-11 17:53:55 -0300

    Update router.dart

--- lib/src/routes/router.dart
+++ lib/src/routes/router.dart
@@ -5,25 +5,15 @@ import 'package:estruturabasica/src/screens/pais/list.dart';
 import 'package:estruturabasica/src/screens/pais/pais_form.dart';
 import 'package:flutter/material.dart';
 
-Route<dynamic> generateRoute(RouteSettings settings){
-  switch (settings.name) {
-    case HomeViewRoute:
-      return MaterialPageRoute(builder: (context) => MyHomePage(title: "Escolinha"));
-    case ListPaisViewRoute:
-      return MaterialPageRoute(builder: (context) => ListPage());
-    case CadPaisViewRoute:
-      return MaterialPageRoute(builder: (context) => PaisForm());
-    default:
-      return MaterialPageRoute(builder: (context) => MyHomePage(title: "Escolinha"));
-  }
-}
-//esse aqui é o certo
 //utilizando o arguments -> nomePage(arguments['id']);
+//criação das rotas nomeadas
 RouteFactory rotas(){
   return (settings) {
       //final Map<Object, dynamic> arguments = settings.arguments;
+      // No editar passo um objeto, então aqui na rota defino que como argumento espero um objeto
       Object obj = settings.arguments;
       Widget screen;
+      //
       switch (settings.name) {
         case HomeViewRoute:
           screen = MyHomePage(title: "Escolinha");
