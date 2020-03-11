0f6a4577943e149a09d6c1bf169d0b1ac74664f2
Paulo Saldanha da Silva <paulosaldanhadasilva@gmail.com>
2020-03-11 17:48:38 -0300

    Update routing_constants.dart

--- lib/src/routes/routing_constants.dart
+++ lib/src/routes/routing_constants.dart
@@ -1,3 +1,4 @@
+//Arquivo para definição de rotas, para manter em um unico lugar
 const String HomeViewRoute = '/';
 const String ListPaisViewRoute = 'listpais';
 const String CadPaisViewRoute = 'cadpais';
