7bff8ce83f0c3e03c5236b71d1b2291cbc936003
krausecloudcrm <lucaskrause@cloudcrm.tech>
2021-01-18 16:15:29 -0300

    thinkdata funcionando

--- lib/src/services/thinkdata_service.dart
+++ lib/src/services/thinkdata_service.dart
@@ -4,10 +4,12 @@ import 'package:estruturabasica/src/models/user_thinkdata.dart';
 import 'package:estruturabasica/src/services/auth_service.dart';
 import 'package:http/http.dart' as http;
 
-
 Future<UserThinkdata> getUserThinkData(String documento) async {
   String bearerToken = await AuthService().checkIfUserIsLoggedIn();
   var client = http.Client();
+  print(documento);
+  documento = documento.replaceAll("\/", "");
+  print(documento);
 
   try {
     var response = await client.get(
@@ -19,7 +21,6 @@ Future<UserThinkdata> getUserThinkData(String documento) async {
         });
 
     return UserThinkdata.fromMap(jsonDecode(response.body));
-
   } finally {
     client.close();
   }
